9213adc7363df056fa55f4b4c34975d76baa3e77
Muhammad Mairaj <rayyanmairaj2@gmail.com>
2024-07-30 03:49:37 +0800

    Added styles and created custom widgets

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-
+from custom_widgets import *
 
 class App(tk.Tk):
     def __init__(self):
@@ -10,17 +10,24 @@ class App(tk.Tk):
         self.title("Material Calculator")
         self.geometry("400x300")
 
+        self._create_styles()
         self._create_widgets()
+
+    def _create_styles(self):
+        # Create a style object
+        style = ttk.Style()
+
+        # Define a style for Frames
+        style.configure("TFrame", background='#A5C4D4')
 
 
     def _create_widgets(self):
-        # Create a notebook (tab control)
         self.notebook = ttk.Notebook(self)
         self.notebook.pack(expand=True, fill='both')
 
         # Create frames for each tab
-        self.frame1 = ttk.Frame(self.notebook)
-        self.frame2 = ttk.Frame(self.notebook)
+        self.frame1 = ttk.Frame(self.notebook, style="TFrame")
+        self.frame2 = ttk.Frame(self.notebook, style="TFrame")
 
         # Add frames to the notebook
         self.notebook.add(self.frame1, text="Enter Values")
@@ -31,28 +38,28 @@ class App(tk.Tk):
         self._create_tab2_widgets()
 
     def _create_tab1_widgets(self):
-        label = ttk.Label(self.frame1, text="This is Tab 1")
+        label = CustomLabel(self.frame1, text="This is Tab 1")
         label.pack(pady=20)
 
-        button = ttk.Button(self.frame1, text="Button in Tab 1")
+        button = CustomButton(self.frame1, text="Button in Tab 1")
         button.pack(pady=20)
 
-        def on_button1_click():
+        def on_button_click():
             messagebox.showinfo("Info", "Button in Tab 1 clicked!")
 
-        button.config(command=on_button1_click)
+        button.config(command=on_button_click)
 
     def _create_tab2_widgets(self):
-        label2 = ttk.Label(self.frame2, text="This is Tab 2")
-        label2.pack(pady=20)
+        label = CustomLabel(self.frame2, text="This is Tab 2")
+        label.pack(pady=20)
 
-        button2 = ttk.Button(self.frame2, text="Button in Tab 2")
-        button2.pack(pady=20)
+        button = CustomButton(self.frame2, text="Button in Tab 2")
+        button.pack(pady=20)
 
-        def on_button2_click():
+        def on_button_click():
             messagebox.showinfo("Info", "Button in Tab 2 clicked!")
 
-        button2.config(command=on_button2_click)
+        button.config(command=on_button_click)
 
 
 if __name__ == "__main__":
