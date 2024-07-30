2066c1f07738bfed75634112fa92e7e4d395692a
Muhammad Mairaj <rayyanmairaj2@gmail.com>
2024-07-31 06:12:03 +0800

    added @property and @property.setter, added default material length changed return types from list to tuple

--- material_calculator/calculator.py
+++ material_calculator/calculator.py
@@ -1,24 +1,36 @@
+from .constants import MATERIAL_LENGTH
+
+
 class Calculator:
+    default_material_length = MATERIAL_LENGTH
+
     def __init__(self, material_length: int, items: dict, method: str | None = None) -> None:
-        self._material_length: int = material_length
+        """
+        set `material_length` < 0 for default material length
+        """
+        self._material_length: int = material_length if material_length >= 0 else self.default_material_length
         self._items: dict = items
         self._item_count: int = sum(self._items.values())
         self._method: str | None = method
 
-    def get_material_length(self) -> int:
+    @property
+    def material_length(self) -> int:
         return self._material_length
 
-    def get_items(self) -> dict:
+    @property
+    def items(self) -> dict:
         return self._items.copy()
 
-    def get_item_count(self) -> int:
+    @property
+    def item_count(self) -> int:
         return self._item_count
 
-    def get_method(self) -> str | None:
+    @property
+    def method(self) -> str | None:
         return self._method
 
-    def solve(self) -> list:
-        match self.get_method():
+    def solve(self) -> tuple[int, int, int]:
+        match self.method:
             # Sort method - uses a dictionary and goes through items in sorted manner
             case "Sort":
                 return self._solve_sort()
@@ -28,21 +40,21 @@ class Calculator:
             case _:
                 return self._solve_preferred()
 
-    def _solve_preferred(self) -> list:
+    def _solve_preferred(self) -> tuple[int, int, int]:
         return self._solve_sort()
 
-    def _solve_sort(self) -> list:
+    def _solve_sort(self) -> tuple[int, int, int]:
         print("\nUSING SORTING METHOD")
 
         required, scrap, excess = (0, 0, 0)
 
-        if self.get_item_count() == 0:
-            return [required, scrap, excess]
+        if self.item_count == 0:
+            return (required, scrap, excess)
 
         required = 1    # will need at least 1
-        cur_material_length = self.get_material_length()
-        num_items = self.get_item_count()
-        items_dict = self.get_items()
+        cur_material_length = self.material_length
+        num_items = self.item_count
+        items_dict = self.items
         while num_items >= 1:
             removed = False
 
@@ -62,7 +74,7 @@ class Calculator:
 
         excess = cur_material_length
 
-        return [required, scrap, excess]
+        return (required, scrap, excess)
 
-    def _solve_adapted_sort(self) -> list:
+    def _solve_adapted_sort(self) -> tuple[int, int, int]:
         raise NotImplementedError
